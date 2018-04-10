13c40266a93efeec5672c5e2bffb62cc8ec900d5
Amardeep Singh <amar5899deep@gmail.com>
2018-04-10 19:23:45 +0530

    commit

--- project.c
+++ project.c
@@ -1,26 +1,27 @@
-#include <stdio.h>
+#include<stdbool.h>
+#include<stdio.h>
+#include<stdlib.h>
 struct process{
     int priority;
     int burst_time;
     int tt_time;
-    int total_time=0;
+    int total_time;
 };
-
 struct queues{
     int priority_start;
     int priority_end;
-    int total_time=0;
-    int length = 0;
-    process *p;
-    bool executed = false;
+    int total_time;
+    int length;
+    struct process *p;
+    bool executed;
 };
-
-bool notComplete(queues q[]){
+bool notComplete(struct queues q[]){
     bool a=false;
     int countInc=0;
-        for(int i=0;i<3;i++){
+    int i,j;
+        for(i=0;i<3;i++){
                 countInc=0;
-            for(int j=0;j<q[i].length;j++){
+            for(j=0;j<q[i].length;j++){
                 if(q[i].p[j].burst_time != 0){
                     a=true;
                 }
@@ -35,28 +36,24 @@ bool notComplete(queues q[]){
         }
         return a;
 }
-
-
-
-void sort_ps(queues q){
-    //Queue q has to be sorted according to priority of processes
-    for(int i=1;i<q.length;i++){
-        for(int j=0;j<q.length-1;j++){
+void sort_ps(struct queues q){
+	int i,j;
+    for(i=1;i<q.length;i++){
+        for(j=0;j<q.length-1;j++){
             if(q.p[j].priority<q.p[j+1].priority){
-                process temp = q.p[j+1];
+                struct process temp = q.p[j+1];
                 q.p[j+1] = q.p[j];
                 q.p[j] = temp;
             }
         }
     }
 }
-
-
-void checkCompleteTimer(queues q[]){
+void checkCompleteTimer(struct queues q[]){
     bool a = notComplete(q);
-    for(int i=0;i<3;i++){
+    int i,j;
+    for(i=0;i<3;i++){
         if(q[i].executed==false){
-            for(int j=0;j<q[i].length;j++){
+            for(j=0;j<q[i].length;j++){
                 if(q[i].p[j].burst_time!=0){
                     q[i].p[j].total_time+=1;
                 }
@@ -65,11 +62,8 @@ void checkCompleteTimer(queues q[]){
         }
     }
 }
-
 int main(){
-
-    //Initializing 3 queues
-    queues q[3];
+    struct queues q[3];
     q[0].priority_start = 7;
     q[0].priority_end = 9;
     q[1].priority_start = 4;
@@ -78,29 +72,28 @@ int main(){
     q[2].priority_end = 3;
 
     int no_of_processes,priority_of_process,burst_time_of_process;
-    //Prompt User for entering Processes and assigning it to respective queues.
     printf("Enter the number of processes\n");
-    scanf("%d",no_of_processes);
-    process p1[no_of_processes];
+    scanf("%d",&no_of_processes);
+    struct process p1[3];
+    int i,j;
 
-    for(int i=0;i<no_of_processes;i++){
+    for(i=0;i<no_of_processes;i++){
         printf("Enter the priority of the process\n");
-        scanf("%d",priority_of_process);
+        scanf("%d",&priority_of_process);
         printf("Enter the burst time of the process\n");
-        scanf("%d",burst_time_of_process);
+        scanf("%d",&burst_time_of_process);
         p1[i].priority = priority_of_process;
         p1[i].burst_time = burst_time_of_process;
         p1[i].tt_time = burst_time_of_process;
-        for(int j=0;j<3;j++){
+        for(j=0;j<3;j++){
         if(q[j].priority_start<=priority_of_process && priority_of_process<=q[j].priority_end){
             q[j].length++;
         }
         }
     }
-
-    for(int i =0;i<3;i++){
+    for(i =0;i<3;i++){
         int len = q[i].length;
-        q[i].p = new process[len];
+        q[i].p = malloc(sizeof *q);
     }
 
 
@@ -108,8 +101,8 @@ int main(){
     int b=0;
     int c=0;
 
-    for(int i =0;i<3;i++){
-        for(int j=0;j<no_of_processes;j++){
+    for(i =0;i<3;i++){
+        for(j=0;j<no_of_processes;j++){
             if((q[i].priority_start<=p1[j].priority) && (p1[j].priority<=q[i].priority_end)){
                 if(i==0){
                     q[i].p[a++] = p1[j];
@@ -126,16 +119,14 @@ int main(){
     }
 
     a--;b--;c--;
-    for(int i=0;i<3;i++){
-        //printf("Queue "(i+1));
-        for(int j=0;j<q[i].length;j++){
-          //  printf(q[i].p[j].priority("->");
+    for(i=0;i<3;i++){
+        printf("Queue ",i+1);
+        for(j=0;j<q[i].length;j++){
+          printf("%d\n",q[i].p[j].priority);
         }
         printf("NULL\n");
     }
 
-
-    //While RR on multiple queues is not complete, keep on repeating
     int timer = 0;
     int l =-1;
     while(notComplete(q)){
@@ -146,36 +137,30 @@ int main(){
             }
             timer = 0;
         }
-
-        //Process lth queue if its already not executed
-        //If its executed change the value of l
         if(q[l].executed == true){
-              //  printf("Queue "(l+1(" completed\n");
+               printf("Queue %d completed %d\n",l+1);
             l+=1;
             if(l>=3){
                 l=l%3;
             }
             continue;
-        }
-
-        //Finally you now have a queue which is not completely executed
-        //Process the incomplete processes over it
+    }
 
         if(l==0){
-            //printf("Queue "(l+1(" in hand\n")));
-            //Round Robin Algorithm for q=4
+            printf("Queue %d in hand %d\n",l+1);
+    
             int rr_timer = 4;
-            for(int i=0;i<q[l].length;i++){
+            for(i=0;i<q[l].length;i++){
                 if(q[l].p[i].burst_time==0){
                         continue;
                 }
                 while(rr_timer>0 && q[l].p[i].burst_time!=0 && timer!=10){
-					//printf("Executing queue 1 and "(i+1(" process for a unit time. Process has priority of "(q[l].p[i].priority("\n")))));
+					printf("Executing queue 1 and %d process for a unit time. Process has priority of %d\n",i+1,q[l].p[i].priority);
                     q[l].p[i].burst_time--;
                     checkCompleteTimer(q);
                     rr_timer--;
                     timer++;
-                    //printf(l("\t"(timer));
+                    printf("timer %d\n",l);
                     printf("\n");
                 }
                 if(timer == 10){
@@ -197,19 +182,18 @@ int main(){
 
 
         else if(l==1){
-            //printf("Queue "(l+1(" in hand\n")));
+            printf("Queue %d in hand %d\n",l+1);
             sort_ps(q[l]);
-            //Priority Scheduling
-            for(int i=0;i<q[l].length;i++){
+            for(i=0;i<q[l].length;i++){
                 if(q[l].p[i].burst_time==0){
                         continue;
                 }
                 while(q[l].p[i].burst_time!=0 && timer!=10){
-                    //printf("Executing queue 2 and "(i+1(" process for a unit time. Process has priority of "(q[l].p[i].priority("\n")))));
+                    printf("Executing queue 2 and %d process for a unit time. Process has priority of %d\n",i+1,q[l].p[i].priority);
                     q[l].p[i].burst_time--;
                     checkCompleteTimer(q);
                     timer++;
-                    //printf(l("\t"(timer(endl))));
+                    printf("timer %d\n",l);
                 }
                 if(timer == 10){
                     break;
@@ -221,14 +205,13 @@ int main(){
             }
         }
         else{
-            //printf("Queue "(l+1(" in hand\n")));
-            //FCFS
-            for(int i=0;i<q[l].length;i++){
+            printf("Queue %d in hand %d\n",l+1);
+            for(i=0;i<q[l].length;i++){
                 if(q[l].p[i].burst_time==0){
                         continue;
                 }
                 while(q[l].p[i].burst_time!=0 && timer!=10){
-                    //printf("Executing queue 3 and "(i+1(" process for a unit time. Process has priority of "(q[l].p[i].priority("\n")))));
+                    printf("Executing queue 3 and %d  process for a unit time. Process has priority of %d\n",i+1,q[l].p[i].priority);
                     q[l].p[i].burst_time--;
                     checkCompleteTimer(q);
 
@@ -244,13 +227,13 @@ int main(){
             }
 
         }
-        //printf("Broke from queue "(l+1("\n")));
+        printf("Broke from queue %d\n",l+1);
     }
 
-    for(int i=0;i<3;i++){
-            //printf("\nTime taken for queue "(i+1(" to execute: "(q[i].total_time("\n")))));
-        for(int j=0;j<q[i].length;j++){
-            //printf("Process "(j+1(" of queue "(i+1(" took "(q[i].p[j].total_time("\n")))))));
+    for(i=0;i<3;i++){
+            printf("\nTime taken for queue %d to execute: %d\n ",i+1,q[i].total_time);
+        for(j=0;j<q[i].length;j++){
+            printf("Process %d of queue %d took %d\n",j+1,i+1,q[i].p[j].total_time);
         }
     }
 
@@ -258,16 +241,16 @@ int main(){
     int sum_wt=0;
 
     printf("\n\nProcess     | Turn Around Time | Waiting Time\n");
-    for(int i=0;i<3;i++){
-            //printf("Queue "(i+1("\n");
-        for(int j=0;j<q[i].length;j++){
-            //printf("Process P"(j+1("\t"(q[i].p[j].total_time("\t\t    "(q[i].p[j].total_time-q[i].p[j].tt_time("\n")))))));
+    for(i=0;i<3;i++){
+            printf("Queue %d\n",i+1);
+        for(j=0;j<q[i].length;j++){
+            printf("Process P %d\n",j+1,q[i].p[j].total_time,q[i].p[j].total_time-q[i].p[j].tt_time);
             sum_tt+=q[i].p[j].total_time;
             sum_wt+=q[i].p[j].total_time-q[i].p[j].tt_time;
         }
     }
 
-    //printf("\n The average turnaround time is : "(sum_tt/no_of_processes(endl)));
-   // printf("\n The average waiting time is : "(sum_wt/no_of_processes(endl)));
+    printf("\n The average turnaround time is :%f\n ",sum_tt/no_of_processes);
+    printf("\n The average waiting time is :%f\n ",sum_wt/no_of_processes);
 
 }
